c544dc26ddfb8da48fa60f3b2e97c459114edec9
LeandroGazoli <leandro.gazoli@outlook.com>
2023-03-31 20:39:34 -0300

    Offcanvas funcionalidades e implementação no menu

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -3,13 +3,13 @@ import React, { useContext, useEffect, useRef, useState } from 'react';
 import styles from './styles.module.scss';
 import { FiInstagram } from 'react-icons/fi';
 import { AiFillGithub, AiFillLinkedin } from 'react-icons/ai';
-import { FaWhatsapp } from 'react-icons/fa';
+import { FaWhatsapp, FaBars } from 'react-icons/fa';
 import Link from 'next/link';
 import { NavContext } from '@/context/NavContext';
 import Offcanvas from '../Offcanvas';
 
 export default function Header() {
-  const [isOffcanvasOpen, setIsOffcanvasOpen] = useState(true);
+  const [isOffcanvasOpen, setIsOffcanvasOpen] = useState(false);
   const { activeLinkId, setActiveLinkId } = useContext(NavContext);
   const navLinks = ['Sobre mim', 'Projetos', 'Conhecimento', 'Fale Comigo'];
 
@@ -45,7 +45,7 @@ export default function Header() {
 
   return (
     <>
-      <nav className={`${styles.container} ${scroll ? styles.scroll : ''}`}>
+      <nav className={`${styles.container} ${scroll ? styles.scroll : ''} ${styles.navigation}`}>
         <div className={styles.wrapper}>
           <Link
             href="#home"
@@ -78,78 +78,52 @@ export default function Header() {
               </li>
             </ul>
           </div>
+          <button
+            className={styles.button_toggle}
+            onClick={handleOpenOrCloseOffcanvas}
+          >
+            <FaBars size={25} />
+          </button>
         </div>
       </nav>
 
-      <div>
-        <button onClick={handleOpenOrCloseOffcanvas}>Open Offcanvas</button>
-        <Offcanvas
-          isOpen={isOffcanvasOpen}
-          onClose={handleOpenOrCloseOffcanvas}
-        >
-          <h2>Offcanvas Content</h2>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-        </Offcanvas>
-      </div>
-
-      <div>
-        <p>teste</p>
-        <p>teste</p>
-        <p>teste</p>
-        <p>teste</p>
-        <p>teste</p>
-        <p>teste</p>
-        <p>teste</p>
-        <p>teste</p>
-        <p>teste</p>
-        <p>teste</p>
-        <p>teste</p>
-        <p>teste</p>
-        <p>teste</p>
-        <p>teste</p>
-        <p>teste</p>
-        <p>teste</p>
-        <p>teste</p>
-        <p>teste</p>
-        <p>teste</p>
-        <p>teste</p>
-        <p>teste</p>
-        <p>teste</p>
-        <p>teste</p>
-        <p>teste</p>
-        <p>teste</p>
-        <p>teste</p>
-        <p>teste</p>
-        <p>teste</p>
-        <p>teste</p>
-        <p>teste</p>
-        <p>teste</p>
-        <p>teste</p>
-        <p>teste</p>
-        <p>teste</p>
-        <p>teste</p>
-        <p>teste</p>
-        <p>teste</p>
-        <p>teste</p>
-        <p>teste</p>
-        <p>teste</p>
-        <p>teste</p>
-        <p>teste</p>
-        <p>teste</p>
-        <p>teste</p>
-        <p>teste</p>
-        <p>teste</p>
-        <p>teste</p>
-        <p>teste</p>
-        <p>teste</p>
-        <p>teste</p>
-        <p>teste</p>
-        <p>teste</p>
-        <p>teste</p>
-        <p>teste</p>
-        <p>teste</p>
-        <p>teste</p>
-      </div>
+      <Offcanvas
+        isOpen={isOffcanvasOpen}
+        onClose={handleOpenOrCloseOffcanvas}
+        size="90%"
+      >
+        <div
+          className={styles.offcanvas_Header}
+          onClick={handleOpenOrCloseOffcanvas}
+        ></div>
+        <div className={`${styles.container} ${styles.offcanvas_body}`}>
+          <div className={styles.mobileMenu}>
+            <ul>{navLinks.map((nav) => renderNavLink(nav))}</ul>
+            <ul className={styles.social_links}>
+              <li>
+                <Link href="">
+                  <FiInstagram size={25} />
+                </Link>
+              </li>
+              <li>
+                <Link href="">
+                  <AiFillGithub size={25} />
+                </Link>
+              </li>
+              <li>
+                <Link href="">
+                  <AiFillLinkedin size={25} />
+                </Link>
+              </li>
+              <li>
+                <Link href="">
+                  <FaWhatsapp size={25} />
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </Offcanvas>
     </>
   );
 }
--- src/components/Offcanvas/index.tsx
+++ src/components/Offcanvas/index.tsx
@@ -1,31 +1,50 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import styles from './styles.module.scss';
 interface OffcanvasProps {
   isOpen: boolean;
   onClose: () => void;
   children: React.ReactNode;
-  size: number;
+  size?: number | string;
 }
 
 const Offcanvas: React.FC<OffcanvasProps> = ({ isOpen, onClose, children, size }) => {
+  const [removeOverlay, setRemoveOverlay] = useState(false);
+
+  useEffect(() => {
+    if (removeOverlay) {
+      setTimeout(() => {
+        setRemoveOverlay(isOpen);
+      }, 350);
+    } else {
+      setRemoveOverlay(isOpen);
+    }
+  }, [isOpen]);
+
+  useEffect(() => {
+    if (isOpen) {
+      document.body.style.overflow = 'hidden';
+      document.body.style.paddingRight = '17px';
+    } else {
+      document.body.style.overflow = '';
+      document.body.style.paddingRight = '';
+    }
+  }, [isOpen]);
+
   const handleClose = () => {
     onClose();
   };
 
   return (
     <>
-      {/* {isOpen && ( */}
-      <div
-        className={[styles.offcanvas, styles['offcanvas-end'], isOpen && styles.show].join(' ')}
-        style={{ width: size | 300 }}
-      >
+      <div className={[styles.offcanvas, styles['offcanvas-end'], isOpen && styles.show].join(' ')}>
         <div className={styles['offcanvas-content']}>{children}</div>
+      </div>
+      {removeOverlay && (
         <div
-          className={styles['offcanvas-overlay']}
+          className={[styles['offcanvas-overlay'], isOpen && styles.show, styles.fade].join(' ')}
           onClick={handleClose}
         ></div>
-      </div>
-      {/* )} */}
+      )}
     </>
   );
 };
